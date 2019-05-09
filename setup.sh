ab1124c6f68ad541b9f537fc40b869943d327d8b
rlucente <rlucente@redhat.com>
2019-05-09 17:45:07 -0400

    remove commands for registry auth and clean up ceph port

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 . $(dirname $0)/demo.conf
 
 PUSHD ${WORKDIR}
-
     # install both oc and aws command line (for OSX)
     brew update && brew upgrade && brew install awscli openshift-cli
 
@@ -21,14 +20,6 @@ PUSHD ${WORKDIR}
     mv -f $APB_BIN ~/NotBackedUp
     rm -f /usr/local/bin/apb
     ln -s ~/NotBackedUp/$APB_BIN /usr/local/bin/apb
-
-    docker login -u="${OREG_USER}" -p="${OREG_PASS}" registry.redhat.io
-
-    oc create secret generic ${SECRET_NAME} \
-        --from-file=.dockerconfigjson=$HOME/.docker/config.json \
-        --type=kubernetes.io/dockerconfigjson
-    oc secrets link default ${SECRET_NAME} --for=pull
-    oc secrets link builder ${SECRET_NAME}
 
     apb registry add opendatahub --type quay --org opendatahub
     apb bundle list
@@ -48,17 +39,16 @@ PUSHD ${WORKDIR}
     echo
     echo "************************************************************************"
     echo "    # Instruct openshift to forward a localhost port to the ceph container"
-    echo "    oc port-forward ceph-nano-0 8000 &"
+    echo "    oc port-forward ceph-nano-0 8080 &"
     echo
     echo "    # Default S3 access key: foo"
     echo "    # Default S3 secret key: bar"
     echo "    export S3_BUCKET_NAME=\"MY-DATA\""
     echo
     echo "    # Now you can specify an endpoint url as the loopback address listening on the forwarded port"
-    echo "    aws --endpoint-url http://127.0.0.1:8000 s3 mb s3://\${S3_BUCKET_NAME}"
-    echo "    aws --endpoint-url http://127.0.0.1:8000 s3 cp \${DATA_FILENAME} s3://\${S3_BUCKET_NAME}"
-    echo "    aws --endpoint-url http://127.0.0.1:8000 s3 ls s3://\${S3_BUCKET_NAME}"
+    echo "    aws --endpoint-url http://127.0.0.1:8080 s3 mb s3://\${S3_BUCKET_NAME}"
+    echo "    aws --endpoint-url http://127.0.0.1:8080 s3 cp \${DATA_FILENAME} s3://\${S3_BUCKET_NAME}"
+    echo "    aws --endpoint-url http://127.0.0.1:8080 s3 ls s3://\${S3_BUCKET_NAME}"
     echo
-
 POPD
 
